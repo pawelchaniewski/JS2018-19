dd25aaed1c0c1866e8d17db2c7c2872b1c86f295
pawelchaniewski <44337388+pawelchaniewski@users.noreply.github.com>
2018-11-29 16:53:35 +0100

    Update HomeWork.js
    Zadanie 2

--- SEM1/Zjazd3/HomeWork.js
+++ SEM1/Zjazd3/HomeWork.js
@@ -1,1 +1,35 @@
 
+// Ex. 2
+
+let obj = (function() {
+  let _val;
+  let setValue = (value) => _val = value;
+  let showValue = function() {
+    (_val === undefined) ? console.log('No Variable!') : null;
+    return _val
+  };
+  
+  let reverseValue = (function() {
+    switch (typeof _val) {
+      case 'string':
+        let parsedToInt = parseInt(_val, 10);
+        if (isNaN(parsedToInt)) { return [..._val].reverse().join(''); };
+        return parsedToInt*(-1);
+      case 'number':
+        return _val*(-1);
+      default:
+        console.log('No Variable! - Cannot reverse!')
+        return _val;
+    }
+  });
+  
+  return {
+    setValue,
+    showValue,
+    reverseValue
+  };
+})();
+
+obj.setValue('haaalo');
+console.log(obj.showValue())
+console.log(obj.reverseValue())
