26f5712f01a83c87792ea731b04c54ff04484f81
Nivo1985 <karol.rogowski@gmail.com>
2019-01-14 08:49:51 +0100

    Zjazd 5

---
+++ SEM1/Zjazd5/7)ClassesAndModules/2)Constructor.js
@@ -0,0 +1,30 @@
+1 
+// class Person {
+//     constructor(id){
+//         this.id = id;
+//     }
+// }
+
+// let me = new Person(1);
+// console.log(me);
+
+//2
+// class Person {
+//     constructor(id){
+//         personId = id;
+//     }
+// }
+
+// let me = new Person(1);
+// console.log(me);
+
+
+//3
+// class Person {
+//     constructor(id){
+//         let personId = id;
+//     }
+// }
+
+// let me = new Person(1);
+// console.log(me);
---
+++ SEM1/Zjazd5/7)ClassesAndModules/3)Methods.js
@@ -0,0 +1,31 @@
+//1
+// class Person {
+//     constructor(id){
+//         this.id = id;
+//         //this.karolFunc = () => (console.log("KAROL"));
+//         //this.showInfo = () => (console.log("KAROL"));
+//     }
+//     showInfo(){
+//         return `Person Id: ${this.id}`
+//     }
+// }
+
+// let me = new Person(1);
+// console.log(me);
+// console.log(me.showInfo());
+
+//2
+// class Person {
+//     constructor(id, firstName, lastName){
+//         this.id = id;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+//     showInfo(){
+//         return `${this.firstName + ' ' + this.lastName} Id: ${this.id}`
+//     }
+// }
+
+// let me = new Person(1,'Karol','Rogowski');
+// console.log(me);
+// console.log(me.showInfo());
