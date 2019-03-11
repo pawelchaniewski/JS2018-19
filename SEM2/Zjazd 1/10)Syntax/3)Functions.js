d38d5aa0e094bad7a75704b0de31c08fcbf46bb0
Nivo1985 <karol.rogowski@gmail.com>
2019-03-11 11:16:36 +0100

    Add files via upload

---
+++ SEM2/Zjazd 1/10)Syntax/3)Functions.js
@@ -0,0 +1,23 @@
+//functions
+
+// function declarationFunc(){
+//     console.log('declarationFunc');
+// }
+// declarationFunc();
+
+
+// declarationFunc();
+// function declarationFunc(){
+//     console.log('declarationFunc');
+// }
+
+
+// var expresionFunc = function(){
+//     console.log('expresionFunc');
+// };
+// expresionFunc();
+
+// expresionFunc();
+// var expresionFunc = function(){
+//     console.log('expresionFunc');
+// };
