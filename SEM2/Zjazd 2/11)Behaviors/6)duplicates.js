a822cb563743058a0357839c2324dbc1afaa3927
Nivo1985 <karol.rogowski@gmail.com>
2019-03-25 10:56:32 +0100

    sem 2 zjazd 2

---
+++ SEM2/Zjazd 2/11)Behaviors/6)duplicates.js
@@ -0,0 +1,10 @@
+// function test(x,y,x){
+//     console.log(x);
+// }
+// test('a','b','c');
+
+// 'use strict';
+// function test(x,y,x){
+//     console.log(x);
+// }
+// test('a','b','c');
---
+++ SEM2/Zjazd 2/11)Behaviors/7)NumberTypes.js
@@ -0,0 +1,26 @@
+// var x = 120,
+// y = 012; // octal
+// console.log(x+y);
+
+// var x = 120,
+// y = 0x12; // hexdec
+// console.log(x+y);
+
+// var x = 120,
+// y = 0xbca; // hexdec
+// console.log(x+y);
+
+// 'use strict';
+// var x = 120,
+// y = 012;
+// console.log(x+y);
+
+// 'use strict';
+// var x = 120,
+// y = 0x12;
+// console.log(x+y);
+
+// 'use strict';
+// var x = 120,
+// y =  parseInt(12,7);
+// console.log(x+y);
