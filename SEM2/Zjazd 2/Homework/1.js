44cbf5aa5fa08aca8472ce613a4bd512c984a2cc
pawelchaniewski <myphone.pawel@gmail.com>
2019-04-05 20:16:34 +0200

    SEM 2 Zjazd 2 zadanie 1

---
+++ SEM2/Zjazd 2/Homework/1.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const MORSE_DICTIONARY = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  " ": "/"
+};
+
+function toMorse(text) {
+  const textAsArray = text.split("");
+  const morseAsArray = textAsArray.map(char => MORSE_DICTIONARY[char]);
+
+  return morseAsArray.join(" ");
+}
+
+function toText(morse) {
+  const morseAsArray = morse.split(" ");
+  const textAsArray = morseAsArray.map(char =>
+    Object.keys(MORSE_DICTIONARY).find(key => MORSE_DICTIONARY[key] === char)
+  );
+
+  return textAsArray.join("");
+}
+
+console.log(toMorse("test in the wild"));
+console.log(toText("- . ... - / .. -. / - .... . / .-- .. .-.. -.."));
