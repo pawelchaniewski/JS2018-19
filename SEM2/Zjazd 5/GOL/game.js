3fcb7986db5cd2c9d01ea2834869bbffebb80256
pawelchaniewski <44337388+pawelchaniewski@users.noreply.github.com>
2019-06-05 19:15:09 +0200

    working game

--- SEM2/Zjazd 5/GOL/game.js
+++ SEM2/Zjazd 5/GOL/game.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const grid = [
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
+let grid = [
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,16 +12,17 @@ const grid = [
 ];
 
 function main(grid) {
+  console.clear();
   console.table(grid);
-  let segment = [];
   let newGrid = [];
-  for (let rowIndex = 0; rowIndex < grid.lenght; rowIndex++) {
+  for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
     const row = grid[rowIndex];
     let newRow = [];
-    for (let columnIndex = 0; columnIndex < row.lenght; columnIndex++) {
-      const columnLenght = grid.lenght;
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const columnLenght = grid.length;
       const cell = grid[rowIndex][columnIndex];
       let neighbours = 0;
+      let newCell = 0;
 
       // on the top
       if (rowIndex - 1 >= 0) {
@@ -31,7 +32,7 @@ function main(grid) {
       }
 
       // on the bottom
-      if (rowIndex + 1 <= row.lenght) {
+      if (rowIndex + 1 < row.length) {
         if (grid[rowIndex + 1][columnIndex] === 1) {
           neighbours++;
         }
@@ -45,7 +46,7 @@ function main(grid) {
       }
 
       // on the right
-      if (columnIndex + 1 <= grid.lenght) {
+      if (columnIndex + 1 < grid.length) {
         if (grid[rowIndex][columnIndex + 1] === 1) {
           neighbours++;
         }
@@ -59,29 +60,58 @@ function main(grid) {
       }
 
       // on the bottom left
-      if (rowIndex + 1 <= row.lenght && columnIndex - 1 >= 0) {
+      if (rowIndex + 1 < row.length && columnIndex - 1 >= 0) {
         if (grid[rowIndex + 1][columnIndex - 1] === 1) {
           neighbours++;
         }
       }
 
       // on the top right
-      if (rowIndex + 1 >= 0 && columnIndex - 1 >= 0) {
-        if (grid[rowIndex + 1][columnIndex - 1] === 1) {
+      if (rowIndex - 1 >= 0 && columnIndex + 1 < grid.length) {
+        if (grid[rowIndex - 1][columnIndex + 1] === 1) {
           neighbours++;
         }
       }
 
       // on the bottom right
-      if (rowIndex + 1 <= row.lenght && columnIndex + 1 >= 0) {
+      if (rowIndex + 1 < row.length && columnIndex + 1 >= 0) {
         if (grid[rowIndex + 1][columnIndex + 1] === 1) {
           neighbours++;
         }
       }
+      
+      if (neighbours < 2) {
+        newCell = 0;
+      } else if (neighbours === 2 && cell === 1) {
+        newCell = 1;
+      } else if (neighbours === 2 && cell === 0) {
+        newCell = 0;
+      } else if (neighbours === 3) {
+        newCell = 1;
+      } else {
+        newCell = 0;
+      }
+      
+    //   newCell = neighbours;
+      newRow.push(newCell);
 
-      console.log(`neightbours ${neighbours}`);
+      // console.log(`neightbours ${neighbours}`);
     }
+    newGrid.push(newRow);
   }
+  return newGrid;
 }
 
-main(grid);
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    });
+}
+async function run() {
+    for (let i = 0; i < 50; ++i) {
+        grid = main(grid);
+        await sleep(100);
+    }
+}
+run();
+
