2c6080185166effe05d363ad34d8c89635923e3c
pawelchaniewski <myphone.pawel@gmail.com>
2019-06-16 19:22:00 +0200

    game of life przygotowanie do sprawdzenia

--- SEM2/Zjazd 5/GOL/game.js
+++ SEM2/Zjazd 5/GOL/game.js
@@ -1,5 +1,29 @@
+/* *******************
+
+Game Of Life - Paweł Chaniewski grupa I
+
+Komentarze w kodzie jedynie ze względu na to,
+że nie mogę osobiście o nim opowiedzieć.
+
+Program składa się z głównej funkcji (main(grid))
+która odpowiada za całą logikę.
+Przyjmuje w parametrze tablicę 2D w której zdefiniowane
+jest pole gry i początkowy stan komórek.
+Stan komórki to 1 (żywa) lub 0 (martwa).
+Rozmiar tabeli może być dowolny.
+
+Program na obecną chwilę działa w konsoli:
+$ node game.js
+
+TODO: Wykorzystać kod w HTML
+
+Dalej w kodzie komentarze z wyjaśnieniami.
+
+******************* */
+
 "use strict";
 
+// Plansza gry ze zdefioniowanymi komórkami (tutaj glider)
 let grid = [
   [0, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 1, 0, 0, 0, 0, 0],
@@ -15,20 +39,36 @@ let grid = [
   [0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+// Główna funkcja
 function main(grid) {
+  // Przebieg gry wyświetlany jest w konsoli
+  // Przed wyświeleniem nowego stanu planszy
+  // Output konsoli jest czyszczony (pseudo animacja)
   console.clear();
   console.table(grid);
+
+  // Plansza za każdym razem jest generowana na nowo
   let newGrid = [];
+
+  // Iteruje po każdym wierszu tabeli
   for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
     const row = grid[rowIndex];
     let newRow = [];
+
+    // Iteruje po każdej kolumnie w danym wierszu
     for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
       const columnLenght = grid.length;
       const cell = grid[rowIndex][columnIndex];
+
+      // Definiuje stany które będą aktualizowane
+      // na podstawie warunków gry
+      // Początkowy stan to martwa komórka bez sąsiadów
       let neighbours = 0;
       let newCell = 0;
 
-      // on the top
+      // Czy nad komórką jest komórka,
+      // jeżeli tak to czy jest żywa
+      // jeżeli żywa => zwiększ ilość sąsiadów
       if (rowIndex - 1 >= 0) {
         if (grid[rowIndex - 1][columnIndex] === 1) {
           neighbours++;
@@ -84,6 +124,11 @@ function main(grid) {
         }
       }
 
+      // W zależności od wyniku poprzednich warunków
+      // Czyli od ilości wykrytych żywych komórek
+      // sąsiadujących z obecnie obsługiwaną,
+      // wybierz czy ma być martwa czy żywa
+      // (zgodnie z zasadami gry w życie)
       if (neighbours < 2) {
         newCell = 0;
       } else if (neighbours === 2 && cell === 1) {
@@ -96,21 +141,29 @@ function main(grid) {
         newCell = 0;
       }
 
-      //   newCell = neighbours;
+      // Buduje nową tabelę - dodaj zaktualizowaną komórkę do wiersza
       newRow.push(newCell);
-
-      // console.log(`neightbours ${neighbours}`);
     }
+
+    // Buduje nową tabelę - dodaj zaktualizowany wiersz do tabeli
     newGrid.push(newRow);
   }
   return newGrid;
 }
 
+// Funkcja pomocnicza - zwykły timeout, aby
+// można było zauważyć poszczególne kroki gry
 function sleep(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
 }
+
+// Funkcja która obsługuje przebieg wyświetlania gry
+// pętla for określa maksymalną liczbę kroków
+// które zostaną wykonane w trakcie jednej gry
+// Fukcja sleep daje możliwość ustalenia częstotliwości
+// odświeżania wyników wyświetlanych w konsoli.
 async function run() {
   for (let i = 0; i < 50; ++i) {
     grid = main(grid);
@@ -118,4 +171,5 @@ async function run() {
   }
 }
 
-// run();
+// Wywołanie gry
+run();
